54a4c5e734c0fc9ee4aaf10cb4833bb0a6849841
tomato_o <156098510+ssafy11thseoul@users.noreply.github.com>
2024-06-11 15:28:02 +0900

    [Silver I] Title: IOIOI, Time: 432 ms, Memory: 29268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Arrays;
 import java.util.Scanner;
 //예시)
@@ -9,51 +10,33 @@ public class Main {
 	static int N;  // N+1게의 I와 N개의 O로 이루어진 문자열 Pn
 	static int M; // S의 길이
 	static String S; // I와 O로 이루어진 문자열
-	static String[] arr; // S를 문자열 배열로 쪼갠 것
-	static String P;
+	static int P; // 패턴의 길이
 	static int cnt = 0; // Pn의 개수
-	// Pn을 만드는 메서드 
-	static String Pn(int N) {
-		String init = "IO";
-		P = init.repeat(N)+"I";
-		return P;
-	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
 		S = sc.next(); // 입력 끝
+		P = 2*N+1;
 		
-		arr = new String[M];
-		arr = S.split("");
-		Pn(N);
-		
-		// arr을 돌면서 조건에 맞으면 cnt++해주기
-		for(int i = 0; i <= M-P.length(); i++) {
-			if(arr[i].equals("I")) {
-				
-				boolean isPattern = false;
-				for(int j = 0; j < P.length(); j++) {
-					if(j %2 == 0) {
-						if(arr[i+j].equals("I")) {
-							isPattern = true;
-						}else {
-							isPattern = false;
-							break;
-						}
-					}else {
-						if(arr[i+j].equals("O")) {
-							isPattern = true;
-						} else {
-							isPattern = false;
-							break;
-						}
-					}
-				}//for j문
-				if (isPattern == true) cnt++;
-			}
-			
-		}// for i문
+		// IOI 패턴 찾기
+        int i = 0;
+        while (i <= M - P) {
+            if (S.charAt(i) == 'I') {
+            	// IOI의 개수 변수
+                int count = 0;
+                while (i + 1 < M && S.charAt(i + 1) == 'O' && i + 2 < M && S.charAt(i + 2) == 'I') {
+                    count++;
+                    if (count == N) {
+                        cnt++;
+                        count--;  // 겹치는 패턴을 고려하여 count 줄임
+                    }
+                    i += 2;
+                }
+            }
+            i++;
+        }
 		System.out.println(cnt);
 		
 	}// main
