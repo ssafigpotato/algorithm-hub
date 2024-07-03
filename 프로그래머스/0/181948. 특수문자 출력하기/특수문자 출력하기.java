fc04474c5691f4cf75cafb9de15fa02df4910ecf
tomato_o <156098510+ssafy11thseoul@users.noreply.github.com>
2024-07-03 17:01:56 +0900

    [level 0] Title: 특수문자 출력하기, Time: 113.64 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // 이스케이프 시퀀스 \ 사용하기
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
