d552d570fac4cc90b50f9c0a098b4293129efa61
tomato_o <156098510+ssafigpotato@users.noreply.github.com>
2025-01-04 22:36:35 +0900

    [level 2] Title: 월별 잡은 물고기 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293260. 월별 잡은 물고기 수 구하기/월별 잡은 물고기 수 구하기.sql
+++ 프로그래머스/2/293260. 월별 잡은 물고기 수 구하기/월별 잡은 물고기 수 구하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
-SELECT count(*) AS FISH_COUNT, MONTH(TIME) AS MONTH
+SELECT COUNT(*) AS FISH_COUNT, MONTH(TIME) AS MONTH
 FROM FISH_INFO
-GROUP BY MONTH -- as로 설정한 이름으로 그룹바이 가능하구나! as를 months라고 하고 이것도 months라고 했더니 됐음 
-ORDER BY MONTH ASC
+GROUP BY MONTH
+ORDER BY MONTH
