39e13da2eecf86cdae326358a9e29d3e0b9d9779
tomato_o <156098510+ssafigpotato@users.noreply.github.com>
2025-02-17 18:21:29 +0900

    [Silver II] Title: 과자 나눠주기, Time: 2184 ms, Memory: 322428 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -7,7 +7,7 @@ public class Main {
     static int M,N;
     static int []snacks;
     static int binarySearch(){
-        int left = 1; // 길이는 최소 1 
+        int left = 1; // 길이는 최소 1
         int right = snacks[snacks.length-1]; // 마지막 원소
         int result = 0;
 
@@ -28,6 +28,7 @@ public class Main {
     static int check(int mid){ // 길이가 mid일 때 나눠줄 수 있는 사람의 수
         int cnt = 0;
         for(int i =0; i <N; i++){ // 각 스낵에 대해서
+            if(snacks[i] < mid) continue; // 작으면 연산할 필요도 없음 <-- 이 코드를 넣으면 효율이 개선될까? 
             cnt += snacks[i] / mid; // snacks[i]를 mid길이로 나눈 몫만큼의 사람에게 배분 가능
             // 예 snacks[i]가 15이고, mid가 7이면 2명 분
         }
