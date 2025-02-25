daeb3afb0bd9e0ef6ffdff999e880c8f4447b7ad
tomato_o <156098510+ssafigpotato@users.noreply.github.com>
2025-02-25 11:31:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 50.93 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i : arr){
+            if(st.isEmpty() || st.peek() != i){
+                st.push(i);
+            }
+        }
+        
+        int[]answer = new int[st.size()];
+        for(int i = answer.length-1; i >= 0; i--){
+            answer[i] = st.pop();
+        }
+        
+        
+        
+
+        return answer;
+    }
+}
