f36212b5ea4bc4728a9948f49cbfb6000bc5b298
tomato_o <156098510+ssafigpotato@users.noreply.github.com>
2025-03-04 14:35:12 +0900

    [Gold V] Title: 치킨 배달, Time: 320 ms, Memory: 22812 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -22,7 +22,7 @@ public class Main {
     // 도시의 치킨거리를 구하기
     static void combination( int at, int depth){
         if(depth == M){ // M개의 치킨집을 선택했으면
-            bfs(); // bfs를 돌려서 각 조합의 치킨거리 중 최솟값 구하기
+            calculate();// bfs를 돌려서 각 조합의 치킨거리 중 최솟값 구하기
             return;
         }
 
@@ -31,48 +31,27 @@ public class Main {
             combination(i+1, depth+1);
         }
     }
-    static void bfs(){
-        Queue<location> que = new LinkedList<>();
-        boolean[][] visited = new boolean[N][N];
-        int[][] distance = new int[N][N]; // 치킨집으로부터 각 집의 거리 저장
-        int total = 0; // 해당 조합에서의 치킨 거리 합
 
-        // 고른 치킨집을 모두 큐에 넣기
-        for(location c : pick){
-            que.offer(c);
-            visited[c.r][c.c] = true;
-            distance[c.r][c.c] = 0;
-        }
+    // 2. 어차피 맨해튼 거리이므로 house와 pick사이의 거리를 구해서 최솟값으로 갱신
+    private static void calculate() {
+        int local_min = 0;
 
-        // 큐에 넣어서 한 번에 돌리므로 각 치킨집으로부터 각 가정집까지 최솟값이 저장됨 -> 3차원 토마토 풀이 참고!
-        // 큐에 들어간 치킨집들부터 우선적으로 계산되기 때문에 더 큰값이 distance에 저장되지 않으려나 하는 우려는 ㄴㄴ
-        // (큐에 들어간 순서가 영향이 없음! 그냥 한번에 배달 시작한다고 생각하면됨)
-        while(!que.isEmpty()){
-            location curr = que.poll();
+        for(location h : house){  // 각 집에 대해서
+            int m_distance = Integer.MAX_VALUE;
 
-            for(int d = 0; d<4; d++){
-                int nr = curr.r + dr[d];
-                int nc = curr.c + dc[d];
-
-                if(nr>= 0 && nr <N && nc>= 0 && nc <N){
-//                    if(!visited[nr][nc] && map[nr][nc] != 2){ // 방문하지 않았고, 치킨집이 아니면
-//                    <-- map[nr][nc] != 2 이 조건때문에 예제 4의 경우 4번째열은 계산되지 않았음!
-                    if(!visited[nr][nc]){
-                        que.offer(new location(nr, nc));
-                        visited[nr][nc] = true;
-                        distance[nr][nc] = distance[curr.r][curr.c] +1;
-
-                    }
-                }
+            for(location c : pick){ // 선택된 치킨집 중 가장 가까운 거리를 저장 -> 치킨 거리
+                int d = Math.abs(h.r - c.r) + Math.abs(h.c - c.c);
+                m_distance = Math.min(m_distance, d);
             }
+
+            local_min += m_distance; // 각 집의 치킨 거리를 더해서 도시의 치킨 거리 구하기
         }
 
-        for(location h : house){  // 각 집에 저장된 거리를 모두 더하면 해당 조합에서 얻은 치킨 거리가 나옴!
-            total += distance[h.r][h.c];
-        }
-        min = Math.min(min, total); // 위에서 계산된 조합들 중 bfs를 돌때마다 가장 최솟값으로 갱신
-
+        // calculate메소드를 돌릴 때마다 더 작은 값으로 갱신 -> 최소값
+        min = Math.min(min, local_min);
     }
+
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -93,8 +72,7 @@ public class Main {
         }// 입력 끝
 
         // 0. 치킨집을 고르는 로직 -> combination
-        // 1. 1일 때 bfs를 돌리기
-        // 2. 각 치킨집 조합에 대해 그 때의 치킨거리를 구하기 -> 최솟값 갱신
+        // 1. 각 치킨집 조합에 대해 그 때의 치킨거리를 구하기 -> 최솟값 갱신
 
         combination( 0,0);
         System.out.println(min);
